82ac1ba86e7797d13287a41604b503906a0a67e2
aldoortiz128 <aldoortiz128@gmail.com>
2021-11-15 15:34:51 +0000

    problems solved

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+function findLongestWord(array) {
+    let longest = '';
+    let length = 0;
+
+for (let i = 0; i <array.length; i++) {
+    if (array[i].length > longest.length) {longest = array[i]}
+
+}
+return longest;
+}
+    
+console.log(findLongestWord(["The","quick","brown", "fox", "jumped", "over", "the", "lazy", "dog"]))
